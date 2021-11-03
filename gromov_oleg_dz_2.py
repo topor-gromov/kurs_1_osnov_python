3d04407def5afa7dbbff75054311c4ee7da213c6
topor <oleg_gromov@mail.ru>
2021-11-03 22:37:19 +0500

    Решение пункта B задания №2 первого занятия.

--- gromov_oleg_dz_2.py
+++ gromov_oleg_dz_2.py
@@ -20,5 +20,32 @@ for numbers in numbers_list:
     if temp_amount % 7 == 0:
         amount = amount + numbers
 
+print(numbers_list)
 print('Сумма чисел списка, удовлетворяющих требованию пункта "a", равна: ',amount)
+###
+
+### Пункт Б
+amount = 0
+temp_number_list = [] #временный список для прибавления 17 к каждому элементу первоначального списка
+
+for numbers in numbers_list:
+    temp_number_list.append(numbers+17)
+
+for numbers in temp_number_list:
+    while_flag = True
+    temp_amount = numbers % 10
+    temp_number = numbers // 10
+    if temp_number > 0:
+        while while_flag:
+            temp_amount = temp_amount + (temp_number % 10)
+            temp_number = temp_number // 10
+            if temp_number == 0:
+                while_flag = False
+    if temp_amount % 7 == 0:
+        amount = amount + numbers
+
+print(temp_number_list)
+print('Сумма чисел списка, удовлетворяющих требованию пункта "b", равна: ',amount)
+
+
 ###
