b39ead6e9267c9895b233afbe5dc98f189928713
topor <oleg_gromov@mail.ru>
2021-11-03 20:46:02 +0500

    Решение первой задачи практического задания первого занятия.

--- gromov_oleg_dz_1.py
+++ gromov_oleg_dz_1.py
@@ -6,7 +6,7 @@ text_result = str(duration_second) + ' сек'
 duration_hour = 0
 duration_day = 0
 if duration_minute != 0:
-   if duration_minute > 59:
+    if duration_minute > 59:
         duration_hour = duration_minute // 60
         duration_minute = duration_minute % 60
         text_result = str(duration_minute) + ' мин ' + text_result
@@ -16,7 +16,7 @@ if duration_minute != 0:
         text_result = str(duration_hour) + ' час ' + text_result
         if duration_day != 0:
             text_result = str(duration_day) + ' дн ' + text_result
-   else:
-       text_result = str(duration_minute) + ' мин ' + text_result
+    else:
+        text_result = str(duration_minute) + ' мин ' + text_result
 
 print(text_result)
